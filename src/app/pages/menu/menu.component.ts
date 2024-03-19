0eeaafa0675b58af618bd2de55732bce6273b3e0
hermannmanuel <hermann.manuel>
2024-03-19 12:44:31 +0100

    Creating links with a dynamic segment

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit, inject, signal } from '@angular/core';
 import { Menu, MenuService } from '../../services/menu.service';
 import { tap } from 'rxjs';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
