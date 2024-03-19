e3673ed3df0ff30eb917a266f564ef7d89c6219f
hermannmanuel <hermann.manuel>
2024-03-19 12:30:15 +0100

    Create a service that provides the restaurant menu + of() operator

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { Menu, MenuService } from '../../services/menu.service';
+import { tap } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+
+  ngOnInit(): void {
+    this.menuService
+     .getMenu()
+     .pipe(
+       tap((menu) => {
+        console.log(menu);
+        this.menuSig.set(menu);
+      })
+    )
+    .subscribe();
+  }
+
+  menuSig = signal<Menu>({} as Menu);
+
+  menuService = inject(MenuService);
 
 }
---
+++ src/app/services/menu.service.ts
@@ -0,0 +1,87 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+export interface Appetizers{
+  id: string;
+  title: string;
+  price: number;
+}
+
+export interface Dish{
+  id: string;
+  title: string;
+  price: number;
+}
+
+export interface Dessert{
+  id: string;
+  title: string;
+  price: number;
+}
+
+export interface Menu {
+  appetizers: Appetizers[];
+  dishes: Dish[];
+  desserts: Dessert[];
+  //foodItems: FoodItem[];
+}
+
+export interface FoodItem{
+  id: string;
+  title: string;
+  price: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MenuService {
+
+  menu: Menu = {
+    appetizers: [
+      {
+        id: 'app1',
+        title: 'Sale Grecque',
+        price: 6.5,
+      },
+      {
+        id: 'app2',
+        title: 'Feuilleté à la Feta',
+        price: 7,
+      },
+
+    ],
+    dishes: [
+      {
+        id: 'dishes1',
+        title: 'Gambas grillé',
+        price: 17,
+      },
+      {
+        id: 'dishes2',
+        title: 'Kebab maison - mix de viandes',
+        price: 15,
+      }
+    ],
+    desserts: [
+      {
+        id: 'des1',
+        title: 'Tiramsu',
+        price: 5,
+      },
+      {
+        id: 'des2',
+        title: 'Baklava',
+        price: 4.5,
+      }
+    ],
+
+  }
+
+  http = inject(HttpClient);
+
+  getMenu(): Observable<Menu> {
+    return of(this.menu);
+  }
+}
