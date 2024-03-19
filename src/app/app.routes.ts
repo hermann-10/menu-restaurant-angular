0d950703f153b776ff8314a5e14fe015dce17e26
hermannmanuel <hermann.manuel>
2024-03-19 12:53:12 +0100

    Route with parameter and query parameter

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { MenuComponent } from './pages/menu/menu.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { EveningsComponent } from './pages/evenings/evenings.component';
 import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
+import { MenudetailsComponent } from './pages/menudetails/menudetails.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'menu', component: MenuComponent },
+  { path: 'menudetails/:id', component: MenudetailsComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'soirees', component: EveningsComponent },
   { path: '**', component: PagenotfoundComponent },
---
+++ src/app/pages/menudetails/menudetails.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menudetails',
+  standalone: true,
+  imports: [],
+  templateUrl: './menudetails.component.html',
+  styleUrl: './menudetails.component.css'
+})
+export class MenudetailsComponent {
+
+}
