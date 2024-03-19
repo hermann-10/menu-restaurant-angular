0d7f8fffa4cb2ba78f127db7ee3ccdabc5882921
hermannmanuel <hermann.manuel>
2024-03-19 10:56:53 +0100

    Managing 404 errors

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { MenuComponent } from './pages/menu/menu.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { EveningsComponent } from './pages/evenings/evenings.component';
+import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'menu', component: MenuComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'soirees', component: EveningsComponent },
+  { path: '**', component: PagenotfoundComponent },
 ];
