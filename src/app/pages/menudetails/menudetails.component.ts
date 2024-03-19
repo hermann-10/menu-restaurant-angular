4436e1eab13c4deb793611ebdca5494ba9ea5757
hermannmanuel <hermann.manuel>
2024-03-19 13:44:06 +0100

    switchMap to switch from one Observable to another

--- src/app/pages/menudetails/menudetails.component.ts
+++ src/app/pages/menudetails/menudetails.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { MenuService } from '../../services/menu.service';
+import { switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-menudetails',
@@ -7,6 +10,26 @@ import { Component } from '@angular/core';
   templateUrl: './menudetails.component.html',
   styleUrl: './menudetails.component.css'
 })
-export class MenudetailsComponent {
+export class MenudetailsComponent implements OnInit {
+  ngOnInit(): void {
+      const id = this.route.snapshot.paramMap.get('id');
+      console.log(id);
+      const foodType = this.route.queryParams
+        .pipe(
+          tap((data) => {
+            console.log({ dataType: data['type'], id: id! });
+        }),
+         switchMap((data: Params) => { //To switch from one type of observable to another -> I use switchMap
+          return this.menuService.getMenuItem(data['type'], id!)
+         })
+        )
+        .subscribe({
+          next: res => {
+            console.log({ insideNext: res });
+          }
+        });
+  }
 
+  route = inject(ActivatedRoute);
+  menuService = inject(MenuService)
 }
